1059804eb1461866a24a4117f3c249fa132fb7d9
leewoonhyoung <wh3619@gmail.com>
2022-04-30 17:00:07 +0900

    2022-04-30 postapi tdd

--- src/main/java/LWH/LWHWEBDESK/web/dto/IndexController.java
+++ src/main/java/LWH/LWHWEBDESK/web/dto/IndexController.java
@@ -1,21 +1,35 @@
 package LWH.LWHWEBDESK.web.dto;
 
+import LWH.LWHWEBDESK.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String  postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto find_id = postsService.findById(id);
+        model.addAttribute("post", find_id);
+        return "posts-update";
     }
 
 
